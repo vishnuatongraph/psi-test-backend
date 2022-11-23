d412d7d35609a1923f7ae5712c2867ce41ebfc91
vishnu sharma <vishnu.sharma@ongraph.com>
2022-11-23 08:01:08 +0530

    model and api change

--- models/user.js
+++ models/user.js
@@ -5,7 +5,6 @@ module.exports = (sequelize, Sequelize) => {
         id: {
           allowNull: false,
           type: Sequelize.INTEGER,
-          autoIncrement: true,
           primaryKey: true,
         },
         name: {
